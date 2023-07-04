36da327a5b7bbdd956443c56665e807abc727dfe
George <george200579@gmail.com>
2023-07-04 18:46:04 +0100

    beginning of menu template

--- menu_package/menu.py
+++ menu_package/menu.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, request
+    Blueprint, render_template, request, jsonify
 )
 
 from menu_package.db import get_db
@@ -13,17 +13,22 @@ def index():
 @bp.route("/search")
 def return_results():
     query = request.args.get("q")
-
     if query == None:
         return None
     
     db = get_db()
-
-    return db.execute(
-        "SELECT *"
+    dishes = db.execute(
+        "SELECT *, type.name AS type_name"
         "  FROM dishes JOIN type ON type.id = dishes.type_id"
-        " ORDER BY menu_number ASC"
+        " WHERE dishes.name LIKE '%'"
+        " ORDER BY menu_number ASC"#,
+        #(query,)
     ).fetchall()
+
+    # https://stackoverflow.com/questions/3286525/return-sql-table-as-json-in-python
+    # 2nd top answer, this guy is very cool.
+    # i may use this if i ever need to return json.
+    return render_template("menu/query.html", dishes=dishes)
 
 
 
