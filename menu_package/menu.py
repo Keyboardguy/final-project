9ce5206d3d865440181577e6b30f12f51d163736
George <george200579@gmail.com>
2023-07-20 22:32:52 +0100

    separated add and delete, need to fix aint vs str bug

--- menu_package/menu.py
+++ menu_package/menu.py
@@ -8,22 +8,6 @@ bp = Blueprint('menu', __name__)
 
 @bp.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        dish_id = request.form.get("dish")
-        if dish_id == None:
-            return 0
-        
-        if "current_dishes" not in session:
-            session["current_dishes"] = []
-        
-        for dish in session["current_dishes"]:
-            if dish["id"] == dish_id:
-                dish["count"] += 1
-                return dish["count"]
-        
-        session["current_dishes"].append({"id": dish_id, "count": 1})
-        return 1
-
     return render_template("menu/index.html")
 
 @bp.route("/search")
@@ -50,6 +34,65 @@ def return_results():
     return render_template("menu/query.html", dishes=dishes)
 
 
+@bp.route("/add", methods=["POST"])
+def add_items():
+    # TODO - I could make a generic function, since add_items and delete_items do nearly the same thing.
+    # Note - The return type must be a string, dict, list, tuple with headers or status, Response instance, or WSGI callable, but it was a int.
+    # So this function and the next one returns string representations of numbers.
+    dish_id, num_to_add = get_form_results()
+    if dish_id == None or num_to_add == None:
+        return "Error"
+    
+    current_dishes = {}
+    new_count = 0
+    for id, value in session["current_dishes"].items():
+        if id == dish_id:
+            value += num_to_add
+            new_count = value
+        current_dishes[id] = value
+        
+    if new_count == 0:
+        current_dishes[dish_id] = num_to_add
+        new_count = num_to_add
 
+    session["current_dishes"] = current_dishes
+    return str(new_count)
+
+
+@bp.route("/delete", methods=["POST"])
+def delete_items():
+    dish_id, num_to_add = get_form_results()
+    if dish_id == None or num_to_add == None:
+        return "Error"
+    
+    current_dishes = {}
+    new_count = 0
+    for id, value in session["current_dishes"].items():
+        if id == dish_id:
+            value += num_to_add
+            if value <= 0:
+                continue
+            new_count = value
+        current_dishes[id] = value
+        
+    session["current_dishes"] = current_dishes
+    return str(new_count)
+
+
+def get_form_results():
+    if "current_dishes" not in session:
+        session["current_dishes"] = {}
+
+    dish_id = request.form.get("dish_id")
+    num_to_add = request.form.get("number")
+
+    try:
+        dish_id = int(dish_id)
+        num_to_add = int(num_to_add)
+    except ValueError:
+        return None, None
+    
+    return dish_id, num_to_add
+    
 
 
--- menu_package/static/menu.js
+++ menu_package/static/menu.js
@@ -8,6 +8,24 @@ search_bar.addEventListener("input", async function() {
 })
 
 function attach_listeners() {
+    function attach_button_listener(button, fetch_url, dish_id, num, output_element) {
+        const form_data = new URLSearchParams();
+        form_data.append("dish_id", dish_id);
+        form_data.append("number", num);
+
+        button.addEventListener("click", async function() {
+            const fetch_promise = await fetch(fetch_url, {
+                method: "POST",
+                body: form_data,
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+            })
+            output_element.innerHTML = await fetch_promise.text();
+        })
+    }
+
+    // make each menu item expandable
     const list_items = document.querySelectorAll("#results > li");
     for (const li of list_items) {
         const name = li.children[0];
@@ -20,5 +38,18 @@ function attach_listeners() {
                 details.classList.add("hidden");
             }
         });
+    }
+
+    // make the buttons on the menu item work
+    const buttons_divs = document.querySelectorAll(".add_delete");
+
+    for (const div of buttons_divs) {
+        const add_button = div.children[0];
+        const delete_button = div.children[1];
+        const span = div.children[2];
+        const dish_id = div.children[3].value;
+
+        attach_button_listener(add_button, "/add", dish_id, 1, span);
+        attach_button_listener(delete_button, "/delete", dish_id, -1, span);
     }
 }
